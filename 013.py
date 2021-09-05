7075fdf0ca86403fa4abb1a6e5d8b96f4a5e2d55
Fabrício de Mello <fabriciomello282@gmail.com>
2021-09-04 23:46:46 -0300

    dia 04/09

---
+++ 013.py
@@ -0,0 +1,5 @@
+#COLOCANDO O NUMERO REAL PARA INTEIRO
+
+import math
+num = float(input('Digite um numero real: '))
+print('O valor digitado foi {} e seu valor inteiro é {}' .format(num, math.trunc(num)))
