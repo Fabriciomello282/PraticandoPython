b579cee9cae7a61f4ac29e0fd9b7bbb0fb345f91
Fabrício de Mello <fabriciomello282@gmail.com>
2021-09-13 23:09:55 -0300

    commit dia 09/09

--- 031.py
+++ 031.py
@@ -1,2 +1,40 @@
 from random import randint
-n = int(input('Vamos Jogar PEDRA PAPEL E TESOURA'))
+itens = ('pedra', 'papel', 'tesoura')
+maquina = randint(0, 2)
+print('Vamos Jogar PEDRA PAPEL E TESOURA')
+jogador = int(input('''Escolha a sua jogada:
+              [0] PEDRA
+              [1] PAPEL
+              [2] TESOURA
+              '''))
+print('-='*15)
+print('JOGADOR: {}'.format(itens[jogador]))
+print('MÁQUINA: {}'.format(itens[maquina]))
+print('-='*15)
+
+if maquina ==0:
+    if jogador ==0:
+        print('EMPATOU!!!')
+    elif jogador ==1:
+        print('VOCÊ GANHOU! Papel embrulha pedra.')
+    elif jogador ==2:
+        print('VOCÊ PERDEU! Tesoura corta papel.')
+
+elif maquina ==1:
+    if jogador ==0:
+        print('VOCÊ PERDEU! Papel embrulha pedra.')
+    elif jogador ==1:
+        print('EMPATOU!!!')
+    elif jogador ==2:
+        print('VOCÊ GANHOU! Tesoura corta papel.')
+
+elif maquina ==2:
+    if jogador ==0:
+        print('VOCÊ GANHOU! Pedra quebra tesoura.')
+    elif jogador ==1:
+        print(' VOCÊ PERDEU! Tesoura corta papel.')
+    elif jogador ==2:
+        print('EMPATOU!!!')
+
+else:
+    print('JOGADA INVÁLIDA!')
